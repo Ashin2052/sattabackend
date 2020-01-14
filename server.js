375a95fb7d5d3ac0d44776636450fc0270257aa6
ashin.mahat <ashin.mahat@palmmind.com>
2020-01-14 23:23:57 +0545

    Af

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const mongoose = require("mongoose");
 const bodyparsers = require("body-parser");
 const routeManager = require("./routes");
 const cors = require("cors");
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
 require("dotenv").config({ path: "variables.env" });
 
-mongoose.connect(process.env.URL || 8080, { useNewUrlParser: true });
-mongoose.connection.on("connected", () =>
-  console.log("mongodb connected successfully.")
-);
+const uri = "mongodb+srv://ashinmahat:myattitude@ashincluster-et3nk.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 mongoose.connection.on("error", error => console.log("connection failed."));
 
 app.use(
@@ -25,4 +29,4 @@ app.use("/", routeManager);
 // app.use('',express.static(__dirname + '/crud/'));
 app.use('', express.static(__dirname + '/sattaking/'));
 
-app.listen(process.env.PORT, () => console.log("server started"));
+app.listen(process.env.PORT || 8080, () => console.log("server started"));
